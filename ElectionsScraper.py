cb7ee18a3fe972d3d4f54e45dace50ce8004d1ae
JKafak97 <dr.jara1@seznam.cz>
2020-11-03 16:09:53 +0100

    Structure set

--- ElectionsScraper.py
+++ ElectionsScraper.py
@@ -6,11 +6,26 @@ from bs4 import BeautifulSoup
 
 def main():
     link = get_link()
+    write_to_csv(link)
+
+
+def get_td_elements():  # get td elements
     pass
 
 
-def get_link():
+def get_soup():  # get BeautifulSoup object
+    pass
+
+
+def get_link():  # get a final list of values
     input_link = input('Insert URL with elections results from your desired district: ')
+
+
+def write_to_csv():
+    pass
+
+
+def et_td_elements():
     pass
 
 
@@ -18,19 +33,24 @@ def name_of_the_csv():
     pass
 
 
-def get_locations_list():
+
+def get_locations_numbers():  # get numbers of locations
     pass
 
 
-def get_locations_numbers():
+def get_locations_names():  # get names of locations
     pass
 
 
-def get_locations_names():
+def get_locations_links(): # get link to location
     pass
 
 
-def get_locations_links():
+def get_parties_names():  # to get names of parties (ODS, ČSSD)
+    pass
+
+
+def make_csv_header():  # make head of tab
     pass
 
 
