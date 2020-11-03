0bdf7e8e58fad98c7ce87d68221e534231209f83
JKafak97 <dr.jara1@seznam.cz>
2020-11-03 15:46:11 +0100

    All "get" function add.

--- ElectionsScraper.py
+++ ElectionsScraper.py
@@ -8,8 +8,29 @@ def main():
     link = get_link()
     pass
 
+
 def get_link():
-    link = input('Insert URL with elections results from your desired district: ')
+    input_link = input('Insert URL with elections results from your desired district: ')
+    pass
+
+
+def name_of_the_csv():
+    pass
+
+
+def get_locations_list():
+    pass
+
+
+def get_locations_numbers():
+    pass
+
+
+def get_locations_names():
+    pass
+
+
+def get_locations_links():
     pass
 
 
